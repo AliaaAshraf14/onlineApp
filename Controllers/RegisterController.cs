da0f03e7d5511fc865b4dd15c7ec0f54d7185a10
Abdulrahman Ashraf <aashraf@meemdev.com>
2024-03-04 15:44:00 +0200

    add register controller

---
+++ Controllers/RegisterController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineFood_Project.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(string clientName)
+        {
+            return Redirect("Home/Index");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -23,6 +23,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Register}/{action=Register}/{id?}");
 
 app.Run();
